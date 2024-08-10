834f0c5b91c9d76305340f7f545dea5d4af627e4
ln <allan.palmu@gmail.com>
2024-08-10 14:09:17 +0300

    normalization fixes, textual inversion compat, renaming, (temporary super verbose debug print)

--- nodes.py
+++ nodes.py
@@ -13,9 +13,9 @@ def maximum_absolute_values(tensors,reversed=False):
     result = tensors[max_abs_idx, torch.arange(tensors.shape[1])]
     return result.reshape(shape[1:])
 
-def get_closest_token_cosine_similarities(single_weight, all_weights, return_scores=False):
-    cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-    scores = cos(all_weights, single_weight.unsqueeze(0).to(all_weights.device))
+def get_closest_token_cosine_similarities(single_embedding, all_embeddings, return_scores=False):
+    cosS = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+    scores = cosS(all_embeddings, single_embedding.unsqueeze(0).to(all_embeddings.device))
     sorted_scores, sorted_ids = torch.sort(scores, descending=True)
     best_id_list = sorted_ids.tolist()
     if not return_scores:
@@ -23,121 +23,153 @@ def get_closest_token_cosine_similarities(single_weight, all_weights, return_sco
     scores_list = sorted_scores.tolist()
     return best_id_list, scores_list
 
-def get_single_cosine_score(single_weight,concurrent_weight):
-    cos = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
-    score = cos(concurrent_weight.unsqueeze(0), single_weight.unsqueeze(0)).item()
+def get_single_cosine_score(single_embedding, other_embedding):
+    cosS = torch.nn.CosineSimilarity(dim=1, eps=1e-6)
+    score = cosS(other_embedding.unsqueeze(0), single_embedding.unsqueeze(0).to(other_embedding.device)).item()
     return score
 
-def refine_token_weight(token_id, all_weights, sculptor_method, sculptor_multiplier):
-    initial_weight = all_weights[token_id]
-    pre_mag = torch.norm(initial_weight)
-    concurrent_weights_ids, scores = get_closest_token_cosine_similarities(initial_weight,all_weights,True)
-    concurrent_weights_ids, scores = concurrent_weights_ids[1:], scores[1:]
+def refine_token_weight(token_id, all_embeddings, sculptor_method, sculptor_multiplier):
+    return refine_embedding_weight(all_embeddings[token_id], all_embeddings, sculptor_method, sculptor_multiplier, original_token_id=token_id)
+
+# returns the sculpted embedding in cpu memory, initial_weight and all_weights should be on the same device but not necessarily 
+def refine_embedding_weight(initial_embedding, all_embeddings, sculptor_method, sculptor_multiplier, original_token_id=None):
+    init_mag = torch.linalg.vector_norm(initial_embedding)
+    score_token_ids, scores = get_closest_token_cosine_similarities(initial_embedding,all_embeddings,True)
+    
+    if original_token_id and score_token_ids[0] == original_token_id:
+        score_token_ids, scores = score_token_ids[1:], scores[1:]
     
     previous_cos_score = 0
+    i = 0
     cos_score = 1
-    iter_num = 0
-    s = []
-    tmp_weights = []
+    near_scores = []
+    near_region = []
+    #ini_w = torch.clone(initial_weight)
     
-    ini_w = torch.clone(initial_weight)
-
-    while previous_cos_score < cos_score:
-        if iter_num > 0:
+    while cos_score > previous_cos_score:
+        print(f"{i}: {previous_cos_score} -> {cos_score}")
+        if i > 0:
             previous_cos_score = cos_score
-        s.append(scores[iter_num])
-        current_weight = all_weights[concurrent_weights_ids[iter_num]]
-        tmp_weights.append(current_weight)
-        vec_sum = torch.sum(torch.stack(tmp_weights),dim=0)
-        cos_score = get_single_cosine_score(ini_w, vec_sum)
-        iter_num += 1
-    del s[-1]
-    del tmp_weights[-1]
+        # get the actual embedding the current score is for
+        candidate_embedding = all_embeddings[score_token_ids[i]]
+        # add it to the embeddings included in the nearby region
+        near_region.append(candidate_embedding)
+        # add the current element of the scores array to the near region score list
+        near_scores.append(scores[i])
+        # get the element-wise sum of the near region embeddings
+        near_region_sum = torch.sum(torch.stack(near_region),dim=0)
+        # update the cosine similarity between the initial embedding and the nearby region sum
+        cos_score = get_single_cosine_score(initial_embedding, near_region_sum)
+        i += 1
 
-    if len(s) <= 1: return initial_weight.cpu(), 0
+    print(f"cos_score stopped increasing at {i-1}: {previous_cos_score} -> {cos_score}")
+    
+    # take the last element out of these
+    del near_scores[-1]
+    del near_region[-1]
+
+    if len(near_region) <= 1: return initial_embedding.cpu(), 0
 
     if sculptor_method == "maximum_absolute":
-        concurrent_weights = torch.stack([ini_w/torch.norm(ini_w)]+[t/torch.norm(t) for i, t in enumerate(tmp_weights)])
-        initial_weight = maximum_absolute_values(concurrent_weights)
-        initial_weight *= pre_mag / torch.norm(initial_weight)
-        return initial_weight.cpu(), len(s)
+        near_region_normalized = torch.stack([initial_embedding.div(init_mag)]+[t.div(torch.linalg.vector_norm(t)) for i, t in enumerate(near_region)])
+        sculpted_embedding = maximum_absolute_values(near_region_normalized)
     elif sculptor_method == "add_minimum_absolute":
-        concurrent_weights = torch.stack([ini_w/torch.norm(ini_w)]+[t/torch.norm(t) for i, t in enumerate(tmp_weights)])
-        initial_weight_min = maximum_absolute_values(concurrent_weights, sculptor_method == "minimum_absolute")
-        initial_weight = ini_w + initial_weight_min * sculptor_multiplier
-        initial_weight *= pre_mag / torch.norm(initial_weight)
-        return initial_weight.cpu(), len(s)
-    
-    concurrent_weights = torch.sum(torch.stack([t * s[i]**2 for i, t in enumerate(tmp_weights)]), dim=0)
-    final_score = get_single_cosine_score(initial_weight,concurrent_weights) * sculptor_multiplier
+        near_region_normalized = torch.stack([initial_embedding.div(init_mag)]+[t.div(torch.linalg.vector_norm(t)) for i, t in enumerate(near_region)])
+        near_region_min = maximum_absolute_values(near_region_normalized, True)
+        sculpted_embedding = initial_embedding + near_region_min.mul(sculptor_multiplier)
+    else:
+        near_region_sum_scoresquared = torch.sum(torch.stack([t.mul(near_scores[i]**2) for i, t in enumerate(near_region)]), dim=0)
+        final_score = get_single_cosine_score(initial_embedding, near_region_sum_scoresquared)
+        print(f"final score: {final_score}")
 
-    if sculptor_method == "backward":
-        initial_weight = initial_weight + concurrent_weights * final_score
-    elif sculptor_method == "forward":
-        initial_weight = initial_weight - concurrent_weights * final_score
+        sculpting = near_region_sum_scoresquared.mul(final_score).mul(sculptor_multiplier)
+        
+        if sculptor_method == "backward":
+            sculpted_embedding = initial_embedding + sculpting
+        elif sculptor_method == "forward":
+            sculpted_embedding = initial_embedding - sculpting
 
-    initial_weight = initial_weight * pre_mag / torch.norm(initial_weight)
-    return initial_weight.cpu(), len(s)
+    sculpted_embedding.mul_(init_mag / torch.linalg.vector_norm(sculpted_embedding)) 
+    return sculpted_embedding.cpu(), len(near_scores)
 
-def vector_sculptor_tokens(clip, text, sculptor_method, token_normalization, sculptor_multiplier):
+def vector_sculptor_text(clip, text, sculptor_method, token_normalization, sculptor_multiplier):
+    return vector_sculptor_tokens(clip, clip.tokenize(text), sculptor_method, token_normalization, sculptor_multiplier)
+
+def vector_sculptor_tokens(clip, tokens, sculptor_method, token_normalization, sculptor_multiplier):
     ignored_token_ids = [49406, 49407, 0]
-    initial_tokens = clip.tokenize(text)
     total_found = 0
     total_replaced = 0
     total_candidates = 0
-    
-    for k in initial_tokens:
-        mean_mag = 0
-        mean_mag_count = 0
-        to_mean_coords = []
-        if k.lower() == "g":
+
+    for stage_id in tokens:
+        stage_found = 0
+        stage_replaced = 0
+        stage_candidates = 0
+        mag_sum = 0
+        mag_count = 0
+        mag_coords = []
+        if stage_id.lower() == "g":
             actual_multiplier = sculptor_multiplier * 4 / 1.5 #2048 to 768, this gives the same effect intensity on both CLIP
         else:
             actual_multiplier = sculptor_multiplier
-        clip_model = getattr(clip.cond_stage_model, f"clip_{k}", None)
-        all_weights = torch.clone(clip_model.transformer.text_model.embeddings.token_embedding.weight).to(device=model_management.get_torch_device())
+        clip_model = getattr(clip.cond_stage_model, f"clip_{stage_id}", None)
+        
+        all_embeddings = torch.clone(clip_model.transformer.text_model.embeddings.token_embedding.weight).to(device=model_management.get_torch_device())
         if token_normalization == "mean of all tokens":
-            all_mags = torch.stack([torch.norm(t) for t in all_weights])
-            mean_mag_all_weights = torch.mean(all_mags, dim=0).item()
-        for x in range(len(initial_tokens[k])):
-            for y in range(len(initial_tokens[k][x])):
-                token_id, attn_weight = initial_tokens[k][x][y]
+            all_mags = torch.stack([torch.linalg.vector_norm(t) for t in all_embeddings])
+            mean_mag_all_embeddings = torch.mean(all_mags, dim=0).item()
+
+        for x in range(len(tokens[stage_id])):
+            for y in range(len(tokens[stage_id][x])):
+                token, attn_weight = tokens[stage_id][x][y]
+
+                if isinstance(token, torch.Tensor):
+                    token_id = None
+                    embedding = token
+                else:
+                    token_id = token
+                    embedding = all_embeddings[token_id]
+
                 if token_id not in ignored_token_ids and sculptor_multiplier > 0:
-                    total_candidates += 1
-                    new_vector, n_found = refine_token_weight(token_id,all_weights, sculptor_method, actual_multiplier)
+                    stage_candidates += 1
+                    sculpted_embedding, n_found = refine_embedding_weight(embedding, all_embeddings, sculptor_method, actual_multiplier, original_token_id=token_id)
                     if n_found > 0:
-                        total_found += n_found
-                        total_replaced += 1
+                        stage_found += n_found
+                        stage_replaced += 1
                 else:
-                    new_vector = all_weights[token_id]
+                    sculpted_embedding = embedding
+
                 if token_normalization != "none" and y != 0 and token_id != 2:
-                    if token_normalization == "mean" or token_normalization == "mean * attention":
-                        mean_mag += torch.norm(new_vector).item()
-                        mean_mag_count += 1
-                        to_mean_coords.append([x,y])
-                    elif token_normalization == "set at 1":
-                        new_vector /= torch.norm(new_vector)
-                    elif token_normalization == "default * attention":
-                        new_vector *= attn_weight
-                    elif token_normalization == "set at attention":
-                        new_vector /= torch.norm(new_vector) * attn_weight
-                    elif token_normalization == "mean of all tokens":
-                        new_vector /= torch.norm(new_vector) * mean_mag_all_weights
-                initial_tokens[k][x][y] = (new_vector, attn_weight)
-        if (token_normalization == "mean" or token_normalization == "mean * attention") and mean_mag_count > 0:
-            mean_mag /= mean_mag_count
-            for x, y in to_mean_coords:
-                token_weight, attn_weight = initial_tokens[k][x][y]
+                    mag = torch.linalg.vector_norm(sculpted_embedding)
+                    match token_normalization:
+                        case "mean" | "mean * attention":
+                            mag_sum += mag.item()
+                            mag_count += 1
+                            mag_coords.append([x,y])
+                        case "default * attention":
+                            sculpted_embedding.mul_(attn_weight)
+                        case "set at 1":
+                            sculpted_embedding.div_(mag)
+                        case "set at attention":
+                            sculpted_embedding.mul_(attn_weight / mag)
+                        case "mean of all tokens":
+                            sculpted_embedding.mul_(mean_mag_all_embeddings / mag)
+                tokens[stage_id][x][y] = (sculpted_embedding, attn_weight)
+
+        if token_normalization is "mean" or "mean * attention" and mag_count > 0:
+            mean_mag = mag_sum / mag_count
+            for x, y in mag_coords:
+                emb, attn_weight = tokens[stage_id][x][y]
+                emb.mul_(mean_mag / torch.linalg.vector_norm(emb))
                 if token_normalization == "mean * attention":
-                    twm = attn_weight
-                else:
-                    twm = 1
-                token_weight = token_weight / torch.norm(token_weight) * mean_mag * twm
-                initial_tokens[k][x][y] = (token_weight, attn_weight)
-        del all_weights
-    if total_candidates > 0:
-        print(f"total_found: {total_found} / total_replaced: {total_replaced} / total_candidates: {total_candidates} / candidate proportion replaced: {round(100*total_replaced/total_candidates,2)}%")
-    return initial_tokens
+                    emb.mul_(attn_weight)
+                tokens[stage_id][x][y] = (emb, attn_weight)
+
+        del all_embeddings
+
+        if stage_candidates > 0:
+            print(f"stage_id: {stage_id} || total_found: {stage_found} / total_replaced: {stage_replaced} / total_candidates: {stage_candidates} / candidate proportion replaced: {round(100*stage_replaced/stage_candidates,2)}%")
+    return tokens
 
 class vector_sculptor_node:
     def __init__(self):
@@ -161,7 +193,7 @@ class vector_sculptor_node:
     CATEGORY = "conditioning"
 
     def exec(self, clip, text, sculptor_intensity, sculptor_method, token_normalization):
-        sculptor_tokens = vector_sculptor_tokens(clip, text, sculptor_method, token_normalization, sculptor_intensity)
+        sculptor_tokens = vector_sculptor_text(clip, text, sculptor_method, token_normalization, sculptor_intensity)
         cond, pooled = clip.encode_from_tokens(sculptor_tokens, return_pooled=True)
         conditioning = [[cond, {"pooled_output": pooled}]]
         if sculptor_intensity == 0 and token_normalization == "none":
@@ -179,10 +211,10 @@ def add_to_first_if_shorter(conditioning1,conditioning2,x=0):
 
 # cheap slerp / I will bet an eternity doing regex that this is the dark souls 2 camera direction formula
 def average_and_keep_mag(v1,v2,p1):
-    m1 = torch.norm(v1)
-    m2 = torch.norm(v2)
+    m1 = torch.linalg.vector_norm(v1)
+    m2 = torch.linalg.vector_norm(v2)
     v0 = v1 * p1 + v2 * (1 - p1)
-    v0 = v0 / torch.norm(v0) * (m1 * p1 + m2 * (1 - p1))
+    v0 = v0 / torch.linalg.vector_norm(v0) * (m1 * p1 + m2 * (1 - p1))
     return v0
 
 # from  https://discuss.pytorch.org/t/help-regarding-slerp-function-for-generative-model-sampling/32475
@@ -193,8 +225,8 @@ def slerp(high, low, val):
     low = low.reshape(dims[0], -1)
     high = high.reshape(dims[0], -1)
 
-    low_norm = low/torch.norm(low, dim=1, keepdim=True)
-    high_norm = high/torch.norm(high, dim=1, keepdim=True)
+    low_norm = low/torch.linalg.vector_norm(low, dim=1, keepdim=True)
+    high_norm = high/torch.linalg.vector_norm(high, dim=1, keepdim=True)
 
     # in case we divide by zero
     low_norm[low_norm != low_norm] = 0.0
@@ -295,10 +327,10 @@ class norm_mag_node:
             for y in range(len(cond1[x][0])):
                 for z in range(len(cond1[x][0][y])):
                     if cond1[x][0][y][z].shape[0] == 2048:
-                        cond1[x][0][y][z][:768] = cond1[x][0][y][z][:768]/torch.norm(cond1[x][0][y][z][:768]) * torch.norm(empty_cond[0][z%empty_tokens_no][:768])
-                        cond1[x][0][y][z][768:] = cond1[x][0][y][z][768:]/torch.norm(cond1[x][0][y][z][768:]) * torch.norm(empty_cond[0][z%empty_tokens_no][768:])
+                        cond1[x][0][y][z][:768] = cond1[x][0][y][z][:768]/torch.linalg.vector_norm(cond1[x][0][y][z][:768]) * torch.linalg.vector_norm(empty_cond[0][z%empty_tokens_no][:768])
+                        cond1[x][0][y][z][768:] = cond1[x][0][y][z][768:]/torch.linalg.vector_norm(cond1[x][0][y][z][768:]) * torch.linalg.vector_norm(empty_cond[0][z%empty_tokens_no][768:])
                     else:
-                        cond1[x][0][y][z] = cond1[x][0][y][z]/torch.norm(cond1[x][0][y][z]) * torch.norm(empty_cond[0][z%empty_tokens_no])
+                        cond1[x][0][y][z] = cond1[x][0][y][z]/torch.linalg.vector_norm(cond1[x][0][y][z]) * torch.linalg.vector_norm(empty_cond[0][z%empty_tokens_no])
         return (cond1,)
 
 class conditioning_merge_clip_g_l:
